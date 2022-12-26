98f6723c57e466694c9450019f532a3c8dec217e
jlkesh <john.lgd65@gmail.com>
2022-12-26 14:43:14 +0500

    HMap created

---
+++ src/main/java/collectionsframework/beforegenerics/dictionary/HMap.java
@@ -0,0 +1,76 @@
+package collectionsframework.beforegenerics.dictionary;
+
+import java.util.Objects;
+
+public class HMap<K, V> {
+    public static final int DEFAULT_CAPACITY = 16;
+    private Node<K, V>[] table;
+
+    public HMap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public HMap(int initialCapacity) {
+        this.table = new Node[initialCapacity];
+    }
+
+    public V put(K key, V value) {
+        var newNode = new Node<>(hash(key), key, value);
+        int index = index(key);
+        var node = table[index];
+        if (node == null) {
+            table[index] = newNode;
+            return null;
+        } else {
+            while (node != null) {
+                if (node.hash == hash(key) && Objects.equals(node.key, key)) {
+                    V oldValue = node.value;
+                    node.value = value;
+                    return oldValue;
+                }
+                node = node.next;
+            }
+            node.next = newNode;
+            return value;
+        }
+    }
+
+    public V get(K key) {
+        var node = table[index(key)];
+        if (node == null)
+            return null;
+        V value = null;
+        while (node != null) {
+            if (node.hash == hash(key) && Objects.equals(node.key, key)) {
+                return node.value;
+            }
+            value = node.value;
+            node = node.next;
+        }
+        return value;
+    }
+
+
+    private int hash(K key) {
+        return (key.hashCode() >>> 16);
+    }
+
+    private int index(K key) {
+        return hash(key) % DEFAULT_CAPACITY;
+    }
+
+
+    private static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+
+        Node<K, V> next;
+
+        public Node(int hash, K key, V value) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ src/main/java/collectionsframework/beforegenerics/dictionary/HMapTest.java
@@ -0,0 +1,11 @@
+package collectionsframework.beforegenerics.dictionary;
+
+public class HMapTest {
+    public static void main(String[] args) {
+        var hMap = new HMap<String, String>();
+        System.out.println(hMap.put("java", "1996-01-23"));
+        System.out.println(hMap.put("java", "1996-01-08"));
+        System.out.println(hMap.put("python", "1989-12-21"));
+        System.out.println(hMap.get("java"));
+    }
+}
